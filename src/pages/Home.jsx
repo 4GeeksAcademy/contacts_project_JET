e42045ae57ac71580b2c57ce2a6be3d151a3c183
JoseEnri <jose.tocasuche@gmail.com>
2025-07-26 01:07:54 +0000

    making more progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import ContactsCard from "../components/ContactsCard.jsx";
 import { Link } from "react-router-dom";
 
-
 export const Home = () => {
-  
-  
-  
-  const { store } = useGlobalReducer();
+  const { store, dispatch } = useGlobalReducer();
+
+  // THIS IS WHERE THE APP STARTS
+  // INITIAL 'GET' STARTS HERE AND GETS ALL DATA FROM SERVER
+  // THEN SEND TO STORE TO UPDATE ALL PAGES
+
+  useEffect(() => {
+    const slug = "JET365";
+    const createAgenda = async () => {
+      try {
+        const response = await fetch(`${store.baseUrl}agendas/${slug}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.status === 201) {
+          console.log("Agenda is mad good my dude");
+        } else if (response.status === 400) {
+          console.log("Aww Man, my bad but this Agenda is already here");
+        } else {
+          console.log(
+            "Whao, that was not the response I expected!",
+            response.status
+          );
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+const fetchContacts =async () => {
+  try {
+    const response = await fetch(`${store.baseUrl}agendas/${slug}/contacts`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+  },
+});
+if(response.ok) {
+  const data = await response.json();
+  dispatch ({type:"SET_CONTACTS", payload: data.contacts})
+} else {
+  console.log("Bro, I couldn't get your contacts son", response.status)
+}
+  }
+  catch (error) {
+        console.log(error);
+      }
+}
+
+
+    const initializeAgenda = async () => {
+      await createAgenda();
+      await fetchContacts();
+    };
+
+    initializeAgenda();
+  }, [store.baseUrl, dispatch])
 
   return (
     <div className="container mt-5">
--- src/store.js
+++ src/store.js
@@ -1,35 +1,44 @@
-export const initialStore=()=>{
-  return{
+export const initialStore = () => {
+  return {
     baseUrl: "https://playground.4geeks.com/contact/",
-    contacts: []
-  }
-}
+    contacts: [],
+  };
+};
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'GET_AGENDA_BY_USER':
+  switch (action.type) {
+    case "GET_AGENDA_BY_USER":
       return {
         ...store,
         agenda: action.payload
-      }
-    }
+      };
 
-    case 'ADD_CONTACT':
+    case "SET_CONTACTS":
       return {
         ...store,
         contacts: action.payload
-      }
+      };
 
-    case 'EDIT_CONTACT':
+    case "ADD_CONTACT":
       return {
         ...store,
-        contacts: action.payload
-      }
+        contacts: [...store.contacts, action.payload],
+      };
 
-      case 'DELETE_CONTACT':
+    case "EDIT_CONTACT":
       return {
         ...store,
-        contacts: action.payload
-      }
+        contacts: store.contacts.map((contact) =>
+          contact.id === action.payload.id ? action.payload : contact
+        ),
+      };
+
+    case "DELETE_CONTACT":
+      return {
+        ...store,
+        contacts: store.contact.filter(
+          (contact) => contact.id !== action.payload
+        ),
+      };
   }
 }
